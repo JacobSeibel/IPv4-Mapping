5e9cce2ae2ee9d1da2c0261bd3a4cd1b6169d78b
JacobSeibel <seibelja@appstate.edu>
2021-01-23 15:11:57 -0500

    Refined the density math to better represent the density of IPs in a region

--- ipv6-map/src/app/_services/marker.service.ts
+++ ipv6-map/src/app/_services/marker.service.ts
@@ -12,9 +12,17 @@ export class MarkerService {
 
   drawIPMarkers(map: L.Map) {
     this.http.get("http://localhost:5000/ipCounts", {observe: 'response', responseType: 'json'}).subscribe((res: any) => {
-      const markers = res.body.result.map((ipCount: any) => { return [ipCount.latitude, ipCount.longitude]})  
+      const result = res.body.result;
+      // Find the maximum density (represented by log(log(count))) in the set
+      const maxDensity = Math.max.apply(Math, result.map((ipCount: any) => { return Math.log(Math.log(ipCount.count+1)+1) }));
+      console.log(maxDensity);
+      const markers = result.map((ipCount: any) => { 
+        // Each point's intensity is defined as maxDensity - density of this point over the maxDensity
+        const density = Math.log(Math.log(ipCount.count+1)+1)
+        return [ipCount.latitude, ipCount.longitude, (maxDensity - density) / maxDensity] 
+      })  
       // TODO: Figure out why 'as any' is needed
-      const heat = (L as any).heatLayer(markers, {}).addTo(map);
+      const heat = (L as any).heatLayer(markers, {gradient: {0.3: 'blue', 0.5: 'lime', .75: 'yellow', 1: 'red'}}).addTo(map);
     });
   }
 }
