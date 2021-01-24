e242bd2bbad17c9c06e26f62cb1b988b98049c58
JacobSeibel <seibelja@appstate.edu>
2021-01-24 12:02:49 -0500

    Added caching and support for updating after source data changes

--- ipv6_map_api/app.py
+++ ipv6_map_api/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 from flask_restful import Api
 from flask_cors import CORS, cross_origin
@@ -10,50 +11,76 @@ import sys
 sys.path.append("..")
 import ipv6_map_api.ipCount_pb2
 import protobuf_to_dict
+from cachetools import cached, TTLCache
+import hashlib
+
 app = Flask(__name__)
+cache = TTLCache(maxsize=1024, ttl=10)
 cors = CORS(app)
 api = Api(app)
 
-ipLocations = []
-ipBlocks = []
+DATA_FILE = "data/GeoLite2-City-Blocks-IPv4.csv"
+BIN_FILE = "data/ipCounts.bin"
 
-ipCounts = ipv6_map_api.ipCount_pb2.IPCounts()
-try:
-    f = open("data/ipCounts.bin", "rb")
-    ipCounts.ParseFromString(f.read())
-    f.close()
-    createNew = False
-except IOError:
-    print("Could not open data/ipCounts.bin. Creating a new one.")
-    createNew = True
+fileHash = hashlib.md5()
+fileHash.update(open(DATA_FILE, 'rb').read())
+cachedIpCountsDict = {}
 
-if createNew:
-    with open('data/GeoLite2-City-Blocks-IPv4.csv', newline='', encoding='utf-8') as csvfile:
-        next(csvfile)
-        blocksReader = csv.reader(csvfile, delimiter=',', quotechar='"')
-        for row in blocksReader:
-            ipBlocks.append({
-                "latitude": row[7],
-                "longitude": row[8],
-            })
+@cached(cache)
+def read_data():
+    global fileHash
+    global cachedIpCountsDict
 
-    ipCountsDF = pd.DataFrame(ipBlocks).pivot_table(index=['latitude', 'longitude'], aggfunc='size')
-    for ipCount in ipCountsDF.items():
-        if ipCount[0][0] != '' and ipCount[0][1] != '':
-            newCount = ipCounts.ipCounts.add()
-            newCount.latitude = float(ipCount[0][0])
-            newCount.longitude = float(ipCount[0][1])
-            newCount.count = ipCount[1]
-    f = open("data/ipCounts.bin", "wb")
-    f.write(ipCounts.SerializeToString())
-    f.close()
+    newHash = hashlib.md5()
+    newHash.update(open(DATA_FILE, 'rb').read())
+    createNew = fileHash.digest() != newHash.digest()
+    fileHash = newHash
+    if not createNew and cachedIpCountsDict != {}:
+        return cachedIpCountsDict
+    else:
+        print(createNew)
 
+    ipCounts = ipv6_map_api.ipCount_pb2.IPCounts()
+    if not createNew:
+        try:
+            f = open(BIN_FILE, "rb")
+            ipCounts.ParseFromString(f.read())
+            f.close()
+            createNew |= False
+        except IOError:
+            print("Could not open " + BIN_FILE + ". Creating a new one.")
+            createNew = True
+
+    if createNew:
+        print("Creating new protocol buffer")
+        ipBlocks = []
+        with open(DATA_FILE, newline='', encoding='utf-8') as csvfile:
+            next(csvfile)
+            blocksReader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            for row in blocksReader:
+                ipBlocks.append({
+                    "latitude": row[7],
+                    "longitude": row[8],
+                })
+
+        ipCountsDF = pd.DataFrame(ipBlocks).pivot_table(index=['latitude', 'longitude'], aggfunc='size')
+        for ipCount in ipCountsDF.items():
+            if ipCount[0][0] != '' and ipCount[0][1] != '':
+                newCount = ipCounts.ipCounts.add()
+                newCount.latitude = float(ipCount[0][0])
+                newCount.longitude = float(ipCount[0][1])
+                newCount.count = ipCount[1]
+        f = open(BIN_FILE, "wb")
+        f.write(ipCounts.SerializeToString())
+        f.close()
     
+    cachedIpCountsDict = protobuf_to_dict.protobuf_to_dict(ipCounts, including_default_value_fields=True)['ipCounts']
+    return cachedIpCountsDict
 
 @app.route("/ipCounts")
 @cross_origin()
 def getIPCounts():
-    ipCountsDict = protobuf_to_dict.protobuf_to_dict(ipCounts, including_default_value_fields=True)['ipCounts']
+    ipCountsDict = read_data()
     result = ipCountsDict
     if "bounds" in request.args:
         bounds = json.loads(request.args["bounds"])
